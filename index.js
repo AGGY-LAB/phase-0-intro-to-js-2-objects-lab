7c5510afd9a6c0ce05b58023cad40dc5980740e6
Aggy <muneeaggy@gmail.com>
2024-07-06 18:14:47 +0300

    MY commit

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee ={
+    name: " Agnes Munee",
+    streetAddress: '00100',
+}
+// console.log(employee)
+
+const updateEmployeeWithKeyAndValue = function(obj,key,value){
+    let newEmployee = {...obj};
+    newEmployee[key]=value;
+    return newEmployee
+}
+console.log(updateEmployeeWithKeyAndValue(employee,"city","Nairobi"));
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue (obj,key,value){
+    obj[key]=value;
+    return obj
+}
+
+const newEmployees=destructivelyUpdateEmployeeWithKeyAndValue(employee,"age",29);
+console.log(newEmployees)
+console.log(employee)
+
+const deleteFromEmployeeByKey = function(obj,key,value){
+    let newE = {...obj};
+    delete newE[key]
+    return newE
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj,key,value){
+    delete obj[key]
+    return obj
+}
+
